ed947f61bf80cb738328318b42f822061368e8d5
khushipatel1101 <im.khushi.543@gmail.com>
2025-07-12 20:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
 
 # ✅ Download required NLTK datasets to /tmp/nltk_data
+
 nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
 nltk.download('wordnet', download_dir=nltk_data_path)
 nltk.download('omw-1.4', download_dir=nltk_data_path)
